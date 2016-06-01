51ff93e21abfad2d9d359e36ae9a6d49c9329717
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-06-01 18:30:45 +0200

    Update aggregateSheetsByMedia.js

--- aggregateSheetsByMedia.js
+++ aggregateSheetsByMedia.js
@@ -1,44 +1,25 @@
-(function aggregateSheetsByMedia(){
-	
-	const toArray=Function.call.bind(Array.prototype.slice);
-	const Clear=Object.setPrototypeOf;
-	const nakedArray=Clear([]);
-	
-	//const Clear=Function.call.bind(Object.setPrototypeOf,null);
-	//const nakedArray2=Clear([]);console.dir(nakedArray2);
-	
-	
-	const m=toArray(document.styleSheets),l=m.length;
-	
-	var i=0, x, p, e, stack, o=Object.setPrototypeOf({'all':nakedArray()},null);
+(function(){
+ const toArray=Function.call.bind(Array.prototype.slice);
 
-	while(i<l){
-		x=m[i];
-		
-		if(x.disabled!==true){
-			p=x.media.mediaText;
-			if(p!==''){	
-				e=(p in o)?o[p]:(o[p]=[[]]);
-			}else{
-				e=o.all;
-			};
-			e[i]=[x];
-		
+ const o={'all':[]}, m=toArray(document.styleSheets), l=m.length;
+
+ var i=0,p,e,x;
+
+ while(i<l){
+	e=m[i++];
+	if(e.disabled!==true){
+		p=e.media.mediaText;
+		if(p!==''){
+			x=o[p]||(o[p]=[]);//(p in o)?o[p]:(o[p]=[]);
+		}else{
+			x=o.all;
 		};
-		i++;
-	};	
-	console.dir(o);
+		x[x.length]=e;
+	};
+ };
+
+ console.clear();
+ console.group('SheetsByMedia');
+ console.dir(o);
+ console.groupEnd();
 })();
-console.info('end');
-
-
-
-			//if(i>e.length){}
-			//stack=e[i];console.dir(stack);
-			//stack[stack.length]=x;			
-
-
-//	
-
-
-		
